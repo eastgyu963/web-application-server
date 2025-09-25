fce13e5e4b11945dcd25dce93aaeef14eeeeed98
eastgyu11 <eastgyu11@naver.com>
2025-09-26 04:30:54 +0900

    요구사항 1 구현 -처음에 fileInputStream에 절대경로로 안주어서 문제발생->절대경로 사용 -이게 index.html이 문자로 되어있는데 바이트 그대로 읽으려고 하니 문제발생->보조스트림 사용

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,19 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
+import java.nio.charset.StandardCharsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
+
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -24,10 +28,39 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            InputStreamReader r = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(r);
+            String firstLine = br.readLine(); //첫번째 라인만 읽기
+            log.info("first line:{}", firstLine);
+            if (firstLine != null) {
+                String[] split = firstLine.split(" ");
+                log.info("request url:{}", split[1]);
+                if (split[1].equals("/index.html")) {
+                    log.info("파일스트림 생성 시작");
+                    FileInputStream fis = new FileInputStream(
+                            "C:\\study\\web-application-server\\webapp\\index.html");
+                    InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+                    BufferedReader br2 = new BufferedReader(isr);
+                    log.info("파일스트림 생성 완료");
+                    String s;
+                    String indexFile = "";
+                    while ((s = br2.readLine()) != null) {
+                        log.info("s:{}", s);
+                        indexFile = indexFile + s;
+                    }
+                    log.info("indexFile:{}", indexFile);
+                    DataOutputStream dos = new DataOutputStream(out);
+                    byte[] body = indexFile.getBytes();
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                }
+            } else {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = "Hello World".getBytes();
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
